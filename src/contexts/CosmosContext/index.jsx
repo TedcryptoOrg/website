2c6adab902740d6f1ab3b392d93333cc39dff4ff
Eric Thomas <bluebird6922@gmail.com>
2022-03-15 14:31:21 +0300

    @keplr-wallet/cosmos package

--- src/contexts/CosmosContext/index.jsx
+++ src/contexts/CosmosContext/index.jsx
@@ -7,7 +7,6 @@ export const CosmosProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [address, setAddress] = useState(undefined);
   const [connected, setConnected] = useState(false);
-  const [contract, setContract] = useState(undefined);
 
   const connectWallet = async () => {
     console.log("connectWallet")
@@ -31,99 +30,12 @@ export const CosmosProvider = ({ children }) => {
         // If the user approves, the chain will be added to the user's Keplr extension.
         // If the user rejects it or the suggested chain information doesn't include the required fields, it will throw an error.
         // If the same chain id is already registered, it will resolve and not require the user interactions.
-        await window.keplr.experimentalSuggestChain({
-          // Chain-id of the Osmosis chain.
-          chainId: Network.chainId,
-          // The name of the chain to be displayed to the user.
-          chainName: "Osmosis mainnet",
-          // RPC endpoint of the chain. In this case we are using blockapsis, as it's accepts connections from any host currently. No Cors limitations.
-          rpc: "https://rpc-osmosis.blockapsis.com",
-          // REST endpoint of the chain.
-          rest: "https://lcd-osmosis.blockapsis.com",
-          // Staking coin information
-          stakeCurrency: {
-              // Coin denomination to be displayed to the user.
-              coinDenom: "OSMO",
-              // Actual denom (i.e. uatom, uscrt) used by the blockchain.
-              coinMinimalDenom: "uosmo",
-              // # of decimal points to convert minimal denomination to user-facing denomination.
-              coinDecimals: 6,
-              // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
-              // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-              // coinGeckoId: ""
-          },
-          // (Optional) If you have a wallet webpage used to stake the coin then provide the url to the website in `walletUrlForStaking`.
-          // The 'stake' button in Keplr extension will link to the webpage.
-          // walletUrlForStaking: "",
-          // The BIP44 path.
-          bip44: {
-              // You can only set the coin type of BIP44.
-              // 'Purpose' is fixed to 44.
-              coinType: 118,
-          },
-          // Bech32 configuration to show the address to user.
-          // This field is the interface of
-          // {
-          //   bech32PrefixAccAddr: string;
-          //   bech32PrefixAccPub: string;
-          //   bech32PrefixValAddr: string;
-          //   bech32PrefixValPub: string;
-          //   bech32PrefixConsAddr: string;
-          //   bech32PrefixConsPub: string;
-          // }
-          bech32Config: {
-              bech32PrefixAccAddr: "osmo",
-              bech32PrefixAccPub: "osmopub",
-              bech32PrefixValAddr: "osmovaloper",
-              bech32PrefixValPub: "osmovaloperpub",
-              bech32PrefixConsAddr: "osmovalcons",
-              bech32PrefixConsPub: "osmovalconspub"
-          },
-          // List of all coin/tokens used in this chain.
-          currencies: [{
-              // Coin denomination to be displayed to the user.
-              coinDenom: "OSMO",
-              // Actual denom (i.e. uatom, uscrt) used by the blockchain.
-              coinMinimalDenom: "uosmo",
-              // # of decimal points to convert minimal denomination to user-facing denomination.
-              coinDecimals: 6,
-              // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
-              // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-              // coinGeckoId: ""
-          }],
-          // List of coin/tokens used as a fee token in this chain.
-          feeCurrencies: [{
-              // Coin denomination to be displayed to the user.
-              coinDenom: "OSMO",
-              // Actual denom (i.e. uosmo, uscrt) used by the blockchain.
-              coinMinimalDenom: "uosmo",
-              // # of decimal points to convert minimal denomination to user-facing denomination.
-              coinDecimals: 6,
-              // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
-              // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-              // coinGeckoId: ""
-          }],
-          // (Optional) The number of the coin type.
-          // This field is only used to fetch the address from ENS.
-          // Ideally, it is recommended to be the same with BIP44 path's coin type.
-          // However, some early chains may choose to use the Cosmos Hub BIP44 path of '118'.
-          // So, this is separated to support such chains.
-          coinType: 118,
-          // (Optional) This is used to set the fee of the transaction.
-          // If this field is not provided, Keplr extension will set the default gas price as (low: 0.01, average: 0.025, high: 0.04).
-          // Currently, Keplr doesn't support dynamic calculation of the gas prices based on on-chain data.
-          // Make sure that the gas prices are higher than the minimum gas prices accepted by chain validators and RPC/REST endpoint.
-          gasPriceStep: {
-              low: 0.01,
-              average: 0.025,
-              high: 0.04
-          }
-        });
+        await window.keplr.experimentalSuggestChain(Network.comdex);
       } catch {
         console.error("Failed to suggest the chain");
       }
 
-      const chainId = Network.chainId;
+      const chainId = Network.comdex.chainId;
 
       // You should request Keplr to enable the wallet.
       // This method will ask the user whether or not to allow access if they haven't visited this website.
@@ -140,6 +52,7 @@ export const CosmosProvider = ({ children }) => {
       const accounts = await offlineSigner.getAccounts();  
       console.log("accounts", accounts)
 
+      setAddress(accounts[0]);
       setConnected(true);
     }
     catch (error) {
--- src/network.js
+++ src/network.js
@@ -1,8 +1,37 @@
-const Network = {
-  chainId: "comdex-1",
-  
-  rpcUrl: "",
+import { Bech32Address } from "@keplr-wallet/cosmos";
 
+const Network = {
+  comdex: {
+    rpc: "https://rpc.comdex.one",
+    rest: "https://rest.comdex.one",
+    chainId: "comdex-1",
+    chainName: "Comdex",
+    stakeCurrency: {
+      coinDenom: "CMDX",
+      coinMinimalDenom: "ucmdx",
+      coinDecimals: 6,
+    },
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: Bech32Address.defaultBech32Config("comdex"),
+    currencies: [
+      {
+        coinDenom: "CMDX",
+        coinMinimalDenom: "ucmdx",
+        coinDecimals: 6,
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "CMDX",
+        coinMinimalDenom: "ucmdx",
+        coinDecimals: 6,
+      },
+    ],
+    features: ["stargate", "ibc-transfer", "no-legacy-stdTx", "ibc-go"],
+    hideInUI: true,
+  },
   tokenId: 1,
 }
 
