c52f3d52635054e8433853520a89f68a6694e3e0
Criptoruim <128837305+Criptoruim@users.noreply.github.com>
2023-09-02 16:58:01 +0100

    PR: Change Team Bio (#44)
    * Ruim and Thomas Pictures
    
    Upload of new member pictures
    
    * Update Team.jsx
    
    * new logo

--- src/components/layouts/Team.jsx
+++ src/components/layouts/Team.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import JoshLopes from "../../assets/images/team/josh_lopes.png";
 import d3k010 from "../../assets/images/team/d3k010.png";
-import Bt from "../../assets/images/team/bt.jpg";
+import ruim from "../../assets/images/team/RUIM.jpg";
+import thomas from "../../assets/images/team/THOMAS.jpg";
 
 const Team = () => {
     return (
@@ -12,7 +13,8 @@ const Team = () => {
                     <h2 className="tf-title style4 mg-bt-38">Our Team</h2>
                     <h5 className="sub-title help-center mg-bt-32 ">
                         Brought together by crypto with only one goal: to bring crypto to more people
-                        We're on the lookout for talented members. Reach out to find out more.
+                        We're on the lookout for talented members. 
+                        Reach out to find out more.
                     </h5>
                 </div>
                 <div className="sc-card-activity style-2 col-12">
@@ -21,19 +23,14 @@ const Team = () => {
                             <img src={JoshLopes} alt="" />
                         </div>
                         <div className="infor">
-                            <h4>Josh_Lopes</h4><br/>
+                            <h4>Luis Lopes</h4><h6> [Co-Founder]</h6><br/>
                             <p>
-                                Luis, 32 years old, is a software engineer and a crypto enthusiast.<br/>
-                                Living in Portugal.<br/>
-                                Having worked +10 Years in e-commerce companies.<br />
-                                Started his own startup in 2018 for digital consultancy.
+                                Luis, a distinguished software engineer that has amassed over a decade of experience within prominent e-commerce enterprises. 
+                                Hailing from Portugal, his entrepreneurial journey commenced in 2018 with the establishment of a digital consultancy startup. 
+                                Luis's ardor for open-source initiatives and a limitless global perspective distinguishes him. With a firm belief in the pervasive potential of Crypto, he eagerly anticipates its profound impact on the world. 
+                                As a Co-Founder of TedCrypto, Luis is instrumental in steering the company toward a future of boundless innovation.
                             </p><br/>
-                            <p>
-                                "I'm very passionated about open source projects and open world
-                                with the right mindset. I do believe in the potential of Crypto in the world and
-                                I'm very excited to see how it will impact the world."
-                            </p><br/>
-                            <h5>Co-Founder of TedCrypto</h5>
+                            <h5>Lead Dev</h5>
                         </div>
                     </div>
                 </div>
@@ -44,17 +41,15 @@ const Team = () => {
                             <img src={d3k010} alt="" />
                         </div>
                         <div className="infor">
-                            <h4>d3k010</h4><br/>
+                            <h4>José Cunha</h4><h6> [Co-Founder]</h6><br/>
                             <p>
-                                JC, healthcare professional, 38 years old, resident in France,
-                                passionate about the financial markets, involved in crypto since late 2018,
-                                self-taught and aggressive consumer of information available on the web.<br/>
-                                Be able to aspire to change
+                                JC, a healthcare professional who is passionately entwined with the dynamics of financial markets. 
+                                His venture into the world of crypto began in late 2018, marked by his self-taught approach and intensive consumption of online resources. 
+                                JC's unwavering commitment to transformation underpins his philosophy. 
+                                Anchored in a creed of focused determination and unwavering dedication, JC, as a Co-Founder of TedCrypto, 
+                                is resolutely shaping the company's trajectory toward remarkable change.
                             </p><br/>
-                            <p>
-                                "The basis of success is based on focus, continuous work and dedication 🙏"
-                            </p><br/>
-                            <h5>Co-Founder of TedCrypto</h5>
+                            <h5>Project Manager</h5>
                         </div>
                     </div>
                 </div>
@@ -62,22 +57,38 @@ const Team = () => {
                 <div className="sc-card-activity style-2 col-12">
                     <div className="content">
                         <div className="media">
-                            <img src={Bt} alt="" />
+                            <img src={ruim} alt="" />
                         </div>
                         <div className="infor">
-                            <h4>BT</h4><br/>
+                            <h4>Rui Lourenço</h4><br/>
                             <p>
-                                BT, crypto enthusiast and passionate about economics and finance. <br/>
-                                Likes innovation and interaction between people
+                                An innate curiosity for technology guided Ruim from an early age, a journey momentarily interrupted by life's demands. 
+                                In 2021, he resurfaced, rekindling his passion and immersing himself in the realm of crypto. 
+                                The establishment of his crypto-focused blog marked a pivotal moment, aligning his path with TedCrypto. 
+                                As the adept orchestrator of social media and communications, Ruim injects the team with contagious enthusiasm.
                             </p><br/>
-                            <p>
-                                "I believe in give more than receive"
-                            </p><br/>
-                            <h5>Blockinvest blogger / Community Manager</h5>
+                            <h5>Govenrance Lead & Comms</h5>
                         </div>
                     </div>
                 </div>
 
+                <div className="sc-card-activity style-2 col-12">
+                    <div className="content">
+                        <div className="media">
+                            <img src={thomas} alt="" />
+                        </div>
+                        <div className="infor">
+                            <h4>Thomas Maas</h4><br/>
+                            <p>
+                                With a background steeped in traditional finance and accounting, Thomas ventured into the world of crypto in mid-2017.
+                                Thomas bridges the chasm between financial prudence and crypto dynamism. 
+                                Apart from working with Tedcrypo he's also the founder of Crypto & Taxes Portugal and co-founder of Crypto Edits.
+                            </p><br/>
+                            <h5>Financer</h5>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </div>
         </section>
