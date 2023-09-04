89fbaa6a5b715ff767f5eef6408caec5f46fb130
Criptoruim <128837305+Criptoruim@users.noreply.github.com>
2023-09-04 20:59:09 +0100

    Update Team.jsx
    preserve text formating

--- src/components/layouts/Team.jsx
+++ src/components/layouts/Team.jsx
@@ -13,7 +13,7 @@ const Team = () => {
                     <h2 className="tf-title style4 mg-bt-38">Our Team</h2>
                     <h5 className="sub-title help-center mg-bt-32 ">
                         Brought together by crypto with only one goal: to bring crypto to more people
-                        We're on the lookout for talented members. 
+                        We're on the lookout for talented members.<br/>
                         Reach out to find out more.
                     </h5>
                 </div>
@@ -24,10 +24,11 @@ const Team = () => {
                         </div>
                         <div className="infor">
                             <h4>Luis Lopes</h4><h6> [Co-Founder]</h6><br/>
-                            <p>
-                                Luis, a distinguished software engineer that has amassed over a decade of experience within prominent e-commerce enterprises. 
+                             <p style={{ whiteSpace: 'pre-line' }}>
+                                Luis, is a distinguished software engineer who has amassed over a decade of experience within prominent e-commerce enterprises. 
                                 Hailing from Portugal, his entrepreneurial journey commenced in 2018 with the establishment of a digital consultancy startup. 
-                                Luis's ardor for open-source initiatives and a limitless global perspective distinguishes him. With a firm belief in the pervasive potential of Crypto, he eagerly anticipates its profound impact on the world. 
+                                Luis's ardor for open-source initiatives and a limitless global perspective distinguishes him. 
+                                With a firm belief in the pervasive potential of Crypto, he eagerly anticipates its profound impact on the world. 
                                 As a Co-Founder of TedCrypto, Luis is instrumental in steering the company toward a future of boundless innovation.
                             </p><br/>
                             <h5>Lead Dev</h5>
@@ -42,8 +43,8 @@ const Team = () => {
                         </div>
                         <div className="infor">
                             <h4>José Cunha</h4><h6> [Co-Founder]</h6><br/>
-                            <p>
-                                JC, a healthcare professional who is passionately entwined with the dynamics of financial markets. 
+                             <p style={{ whiteSpace: 'pre-line' }}>
+                                JC, is a healthcare professional who is passionately entwined with the dynamics of financial markets. 
                                 His venture into the world of crypto began in late 2018, marked by his self-taught approach and intensive consumption of online resources. 
                                 JC's unwavering commitment to transformation underpins his philosophy. 
                                 Anchored in a creed of focused determination and unwavering dedication, JC, as a Co-Founder of TedCrypto, 
@@ -61,7 +62,7 @@ const Team = () => {
                         </div>
                         <div className="infor">
                             <h4>Rui Lourenço</h4><br/>
-                            <p>
+                             <p style={{ whiteSpace: 'pre-line' }}>
                                 An innate curiosity for technology guided Ruim from an early age, a journey momentarily interrupted by life's demands. 
                                 In 2021, he resurfaced, rekindling his passion and immersing himself in the realm of crypto. 
                                 The establishment of his crypto-focused blog marked a pivotal moment, aligning his path with TedCrypto. 
@@ -79,7 +80,7 @@ const Team = () => {
                         </div>
                         <div className="infor">
                             <h4>Thomas Maas</h4><br/>
-                            <p>
+                             <p style={{ whiteSpace: 'pre-line' }}>
                                 With a background steeped in traditional finance and accounting, Thomas ventured into the world of crypto in mid-2017.
                                 Thomas bridges the chasm between financial prudence and crypto dynamism. 
                                 Apart from working with Tedcrypo, he's also the founder of Crypto & Taxes Portugal and co-founder of Crypto Edits.
