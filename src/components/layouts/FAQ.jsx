2d8e62e0c6c6c2d196a842beb39ec7902d5bf832
Criptoruim <128837305+Criptoruim@users.noreply.github.com>
2023-09-05 10:16:46 +0100

    Faq (#49)
    * Update FAQ.jsx
    
    * Update FAQ.jsx
    
    Updated FAQ text with links for Lotto and Cosmos Guardian

--- src/components/layouts/FAQ.jsx
+++ src/components/layouts/FAQ.jsx
@@ -7,32 +7,32 @@ const FAQ = () => {
             {   key: "0",
                 show: "show",
                 title: 'How can I stake with TedCrypto?',
-                text: 'You can stake by going to our network / nodes and select one validator for your tokens. Delegate and start earning! Thanks for your support.'
+                text: 'You can stake your tokens directly from your wallet or you can use our TedLotto app.\n\nWallet: Go to your preferred assets, hit stake and search for Tedcrypto.\nTedlotto: Connect your wallet to [Tedlotto App](http://lotto.tedcrypto.io), choose your preferred network from the home panel and follow the instructions on the Stake tab.\n\nDelegate and start earning! Thanks for your support.'
             },
             {
                 key: "1",
                 title: 'Can I lose crypto by staking?',
-                text: 'Hardly you will lose crypto staking. You can only lose if the node/validator is not working properly and gets jailed. Crypto you stake remains yours and we (validators) don\'t have access to it'
+                text: 'Staking is a way of earning inflation rewards for holding crypto. Hardly you will lose crypto by staking it.\n What can happen is a devaluation of the currency while your tokens are staked/locked. \nWhen you stake your tokens we, validators, don\'t have access to it.'
             },
             {
                 key: "2",
                 title: 'Can I withdraw my crypto?',
-                text: 'You can always withdraw your delegated crypto, but it will take some days to process it depending on the network. Usually is around 15 days but can take more.'
+                text: 'You can choose to withdraw your delegated crypto at any time however, it needs to pass through an unbonding period of 14 to 21 days (depending on chain) before it is available to trade. \nUnbonding period is a security mechanism applied in cosmos-based chains. \nThis mechanism helps secure the chain and also - in case your wallet gets compromised - if some unstakes tokens on your behalf you have this period to do something about it before it gets to the wrong hands (this is what we do at [Cosmos Guardian]{www.cosmosguardian.com})'
             },
             {
                 key: "3",
                 title: 'Can I re-delegate after already delegating my crypto?',
-                text: 'Yes, most of the networks allows you to re-delegate your tokens without having to wait. This is perfect to keep delegation power split between all validators. No validator should have the majority of power as that can compromise governance/voting system and the project!'
+                text: 'Yes, most of the networks allow you to re-delegate your tokens without having to wait for the unbonding period. \nRe-delegation tool is perfect to keep the delegated power split between all validators. \nNo validator should have the majority of power as that can compromise governance and the entire project!'
             },
             {
                 key: "4",
                 title: 'How do I earn crypto?',
-                text: 'Proof of stake, aims to promote staking to validators so you keep the network alive whilst holding to your tokens. This mechanism also allows delegators to earn a % of rewards for the tokens they delegate.'
+                text: 'Proof of stake, aims to promote staking to validators so you keep the network alive whilst holding to your tokens. \nThis mechanism also allows delegators to earn a % of rewards for the tokens they delegate.'
             },
             {
                 key: "5",
                 title: 'What is this "commission"?',
-                text: 'Comissions are the amount of tokens that are taken by the validator in your rewards. This is to cover costs running the nodes/network. We will always charge you the average commission. Some nodes have 0% commission, but not only you can run into problems with it, they also make it unfair for others as running validators do have a cost. They are also excluded from airdrops most of the times.'
+                text: 'Validator Commission is a commission charged by the validator for operational costs. \nThe average Validator Commission is between 5-10% from total rewards, depending on the chain. \nSome Validators might have 0% commission, but not only you can run into problems with it, they can change comission at anytime, so beware of marketing gimmicks. \nTheyre also excluded from airdrops most of the times.'
             },
         ]
     )
