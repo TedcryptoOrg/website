600974f3a4b8de81076170eecb0aae05a723d959
Criptoruim <128837305+Criptoruim@users.noreply.github.com>
2023-09-22 09:47:58 +0100

    Update communities.js
    update static data for communities 
    added reddit and linkedin

--- src/assets/static-data/communities.js
+++ src/assets/static-data/communities.js
@@ -1,5 +1,7 @@
 import telegramIcon from '../images/svg/telegram.svg'
 import discordIcon from '../images/svg/discord-icon.svg'
+import linkedinIcon from '../images/svg/LinkedIn_icon.svg'
+import redditIcon from '../images/svg/reddit_icon.svg'
 
 const communitiesData = [
     {
@@ -12,6 +14,18 @@ const communitiesData = [
         imgAuthor: discordIcon,
         name: "Discord",
         link: 'http://discord.tedcrypto.io',
+        buttonLabel: 'Join Us',
+    },
+    {
+        imgAuthor: linkedinIcon,
+        name: "LinkedIn",
+        link: 'https://www.linkedin.com/company/tedcryptoio',
+        buttonLabel: 'Join Us',
+    },
+    {
+        imgAuthor: redditIcon,
+        name: "Reddit",
+        link: 'https://www.reddit.com/r/tedcrypto/',
         buttonLabel: 'Join Us',
     },
 ]
